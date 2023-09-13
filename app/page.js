51d787fec4f2901f1ee5f3dc69054402983945ea
neuronarchitecture27 <troisiemeoeilagency@gmail.com>
2023-09-13 23:11:11 +0200

    adding animation on scroll feature

--- app/page.js
+++ app/page.js
@@ -1,30 +1,60 @@
 "use client"
-
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
+import Lenis from '@studio-freight/lenis'
 export default function Home() {
-  const [count, setCount] = useState(2)
- useEffect(()=> {
-    if (count < 150 ) {
-        setTimeout(()=> {
-            setCount(count+1)
-        }, 100)
-    }
-    else {
-        setCount(150)
-    }
- })
+  const [count, setCount] = useState(0)
+//  useEffect(()=> {
+//     if (count < 150 ) {
+//         setTimeout(()=> {
+//             setCount(count+1)
+//         }, 100)
+//     }
+//     else {
+//         setCount(150)
+//     }
+//  })
+
+const lenis = new Lenis()
+
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+useEffect(() => {
+  const scrollPx = document.documentElement.scrollTop;
+  console.log(scrollPx);
+  window.addEventListener("scroll", scrollProgress)
+
+  return () => window.removeEventListener('scroll', scrollProgress)
+}, [])
+
+const scrollProgress = () => {
+  const scrollPx = document.documentElement.scrollTop;
+  const winHeightPx = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrollLen = Math.ceil(scrollPx / winHeightPx * 100)
+
+  setCount(scrollLen)
+ }
+
+ 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between w-full h-full">
-      <div >
+    <div className="h-[400vh]">
+      <header className='top-0 sticky min-h-[100vh] flex flex-col items-center justify-center' >
       <Image
               src={`/sequences/ezgif-frame-${count.toString().padStart(3, '0')}.png`}
-              alt="Vercel Logo"
+              alt="Image sequence"
               className="dark:invert"
+           
               fill={true}
               priority
             />
-      </div>
-    </main>
+      </header>
+      <h1 className='z-10'>hello world</h1>
+    </div>
   )
 }
