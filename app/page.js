8d49262ffb995ec36cfbd81ac37a02bebef97e7a
neuronarchitecture27 <troisiemeoeilagency@gmail.com>
2023-09-14 01:18:21 +0200

    added some fixes

--- app/page.js
+++ app/page.js
@@ -4,49 +4,52 @@ import { useEffect, useState } from 'react'
 import Lenis from '@studio-freight/lenis'
 export default function Home() {
   const [count, setCount] = useState(0)
-//  useEffect(()=> {
-//     if (count < 150 ) {
-//         setTimeout(()=> {
-//             setCount(count+1)
-//         }, 100)
-//     }
-//     else {
-//         setCount(150)
-//     }
-//  })
+ useEffect(()=> {
+    if (count < 150 ) {
+        setTimeout(()=> {
+            setCount(count+1)
+        }, 100)
+    }
+    else {
+        setCount(150)
+    }
+ })
 
-useEffect(() => {
-  const lenis = new Lenis()
+// useEffect(() => {
+//   const lenis = new Lenis()
 
-console.log(lenis);
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
+// console.log(lenis);
+// function raf(time) {
+//   lenis.raf(time)
+//   requestAnimationFrame(raf)
+// }
 
-requestAnimationFrame(raf)
-})
+// requestAnimationFrame(raf)
+// })
 
-useEffect(() => {
-  const scrollPx = document.documentElement.scrollTop;
-  console.log(scrollPx);
-  window.addEventListener("scroll", scrollProgress)
+// useEffect(() => {
+//   if (typeof window !== "undefined") {
+  
+//     const scrollPx = document.documentElement.scrollTop;
+//     console.log(scrollPx);
+//     window.addEventListener("scroll", scrollProgress)
+  
+//     return () => window.removeEventListener('scroll', scrollProgress)
+//   }
+// }, [])
 
-  return () => window.removeEventListener('scroll', scrollProgress)
-}, [])
+// const scrollProgress = () => {
+//   const scrollPx = document.documentElement.scrollTop + 2;
+//   const winHeightPx = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+//   const scrollLen = Math.ceil(scrollPx / winHeightPx * 100)
 
-const scrollProgress = () => {
-  const scrollPx = document.documentElement.scrollTop + 2;
-  const winHeightPx = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  const scrollLen = Math.ceil(scrollPx / winHeightPx * 100)
-
-  setCount(scrollLen)
- }
+//   setCount(scrollLen)
+//  }
 
  
   return (
    <div>
-     <div className="h-[400vh] ">
+     <div className=" ">
       <header className='top-0 z-0 sticky min-h-[100vh] flex flex-col items-center justify-center' >
       <Image
               src={`/sequences/ezgif-frame-${count.toString().padStart(3, '0')}.png`}
@@ -58,9 +61,8 @@ const scrollProgress = () => {
             />
       </header>
     </div>
-      <div className='z-10'> 
-      <h1>hello world</h1>
-
+      <div className='absolute top-0 w-96 h-full'> 
+      
       </div>
    </div>
   )
